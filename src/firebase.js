5095a4bb8b4214a61b9421c6dadb901782b02ef3
umar06841 <umar06841@gmail.com>
2026-04-23 11:35:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,14 @@ const firebaseConfig = {
   projectId: "learnova-16738",
   storageBucket: "learnova-16738.firebasestorage.app",
   messagingSenderId: "18480231520",
-  appId: "1:18480231520:web:043972d515a0679ac2bf05"
+  appId: "1:18480231520:web:043972d515a0679ac2bf05",
+  measurementId: "G-Z5BB4BBFYM"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Get services
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
-export default app;
